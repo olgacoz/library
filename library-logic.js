e332ce6c71329978e9373abea9c46ca2650db38d
Olgac Ozsehitoglu <olgacozsehitoglu@gmail.com>
2026-05-04 16:49:05 +0300

    Write code for displaying each book on the page

--- library-logic.js
+++ library-logic.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const libraryNode = document.querySelector('.library');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -9,11 +10,36 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.info = function () {
-  const read = this.read ? 'read' : 'not read yet';
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${read}`;
+  const read = this.read ? 'read' : 'unread';
+  return `<h2 class="book-title">${this.title}</h2>
+         <div class="author">by ${this.author}</div>
+         <div class="pages">${this.pages} pages</div>
+         <button class=${read}>${read}</button>`;
 }
 
 function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
+
+function displayBooks() {
+  for (const book of myLibrary) {
+    const bookNode = document.createElement('div');
+    bookNode.classList.add('book'); // add book class to our node
+
+    bookNode.innerHTML = book.info();
+    libraryNode.appendChild(bookNode);
+  }
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary("1984", "George Orwell", 328, false);
+addBookToLibrary("Dune", "Frank Herbert", 412, true);
+addBookToLibrary("Atomic Habits", "James Clear", 320, true);
+addBookToLibrary("The Pragmatic Programmer", "Andrew Hunt", 352, false);
+addBookToLibrary("Clean Code", "Robert C. Martin", 464, true);
+addBookToLibrary("Eloquent JavaScript", "Marijn Haverbeke", 472, true);
+addBookToLibrary("Kürk Mantolu Madonna", "Sabahattin Ali", 160, true);
+addBookToLibrary("Tutunamayanlar", "Oguz Atay", 724, false);
+
+displayBooks();
