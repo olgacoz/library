fd159fe6e790aae58493b780cf800488bfc6a972
Olgac Ozsehitoglu <olgacozsehitoglu@gmail.com>
2026-05-05 23:59:47 +0300

    Prevent use of object constructor without new keyword

--- library-logic.js
+++ library-logic.js
@@ -47,6 +47,9 @@ newBookForm.addEventListener('submit', (e) => {
 
 function Book(title, author, pages, read) {
   // TODO: Prevent use of constructor without new keyword
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor"); 
+  }
   this.title = title;
   this.author = author;
   this.pages = pages;
